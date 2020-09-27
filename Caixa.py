1d587e47c27e0e8b1c245e4b4cf073f7e3ed4e71
DaviNunes05 <71949419+DaviNunes05@users.noreply.github.com>
2020-09-27 17:29:44 -0300

    Update Caixa.py
    Tarefas à realizar:
    
    Verificar valor de Saldo e reduzi-lo após o Saque.
    
    E atualização de saque.

--- Caixa.py
+++ Caixa.py
@@ -2,35 +2,99 @@ import getpass
 import os
 
 def limpar():
-  os.system('cls' if os.name == 'nt' else 'clear')
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+lista_contas = {
+	'12345' : {
+	'senha' : '123',
+	'nome'  : 'Davi',
+	'valor' : 1200,
+	'admin' : False
+},
+	'654321': {
+	'senha' : '123',
+	'nome'  : 'Davi',
+	'valor' : 3200,
+	'admin' : True
+}
+}
+caixa_cedulas = {
+	'2'   : 0,
+	'5'   : 0,
+	'10'  : 6,
+	'20'  : 7,
+	'50'  : 2,
+	'100' : 50
+}
 
 while True:
-  print("\n**************************")
-  print("*****Caixa Eletrônico*****")
-  print("**************************")
-  print('\nDigite os dados solicitados.')
-  conta_digitada = input('Conta: ')
-  senha_digitada = getpass.getpass('Senha: ')
+	print("\n**************************")
+	print("*****Caixa Eletrônico*****")
+	print("**************************")
+	print('\nDigite os dados solicitados.')
+	conta_digitada = input('Conta: ')
+	senha_digitada = getpass.getpass('Senha: ')
+	
+	if conta_digitada in lista_contas and senha_digitada == lista_contas	[conta_digitada]['senha']:
+		limpar()
+		print('\nTitular: %s' % lista_contas[conta_digitada]['nome'])
+		print('Escolha uma das opções abaixo: ')
+		print('1 - Saldo\n2 - Saque')
+		if lista_contas[conta_digitada]['admin']:
+			print('10 - Incluir Cédulas')
+		opcao = input('Opção: ')
 
-  lista_contas = {
-    '12345' : {
-    'senha' : '123',
-    'nome'  : 'Davi',
-    'valor' : 'R$ 1200'
-    }
-  }
+		if opcao == '1':
+			print('Seu Saldo: %s'% lista_contas[conta_digitada]['valor'])
 
-  if conta_digitada in lista_contas and senha_digitada == lista_contas[conta_digitada]['senha']:
-    limpar()
-    print('\nTitular: %s' % lista_contas[conta_digitada]['nome'])
-    print('Escolha uma das opções abaix: ')
-    print('1 - Saldo')
-    opcao = input('Opção: ')
-    if opcao == '1':
-      print('Seu Saldo: %s'% lista_contas[conta_digitada]['valor'])
-    print()
-  else:
-    print('Conta Inválida.')
-  
-  input('Pressione <Enter> para continuar.')
-  limpar()
+		elif opcao == '10' and lista_contas[conta_digitada]['admin']:
+			qtd_digitada = input('Digite a quantidade de cédulas: ')
+			cedula_digitada = input('Digite a cédula a ser incluída: ')
+			caixa_cedulas[cedula_digitada] += int(qtd_digitada)
+			print(caixa_cedulas)
+
+		elif opcao == '2':
+			valor_saque = int(input('Digite o Valor do Saque: '))
+			cedulas_saque = {}
+
+			if valor_saque // 100 > 0 and valor_saque // 100 <= caixa_cedulas['100']:
+				cedulas_saque['100'] = valor_saque // 100
+				valor_saque = valor_saque - valor_saque // 100 * 100
+			
+			if valor_saque // 50 > 0 and valor_saque // 50 <= caixa_cedulas['50']:
+				cedulas_saque['50'] = valor_saque // 50
+				valor_saque = valor_saque - valor_saque // 50 * 50
+			
+			if valor_saque // 20 > 0 and valor_saque // 20 <= caixa_cedulas['20']:
+				cedulas_saque['20'] = valor_saque // 20
+				valor_saque = valor_saque - valor_saque // 20 * 20
+
+			if valor_saque // 10 > 0 and valor_saque // 10 <= caixa_cedulas['10']:
+				cedulas_saque['10'] = valor_saque // 10
+				valor_saque = valor_saque - valor_saque // 10 * 10
+			
+			if valor_saque // 5 > 0 and valor_saque // 5 <= caixa_cedulas['5']:
+				cedulas_saque['5'] = valor_saque // 5
+				valor_saque = valor_saque - valor_saque // 5 * 5
+
+			if valor_saque // 2 > 0 and valor_saque // 2 <= caixa_cedulas['2']:
+				cedulas_saque['2'] = valor_saque // 2
+				valor_saque = valor_saque - valor_saque // 2 * 2
+
+			if valor_saque != 0:
+				print('Cédulas indisponíves para este valor.')
+
+			else:
+				for acerto_caixa in cedulas_saque:
+					caixa_cedulas[acerto_caixa] -= cedulas_saque[acerto_caixa]
+
+				print('Saque as Notas: ')
+				print(cedulas_saque)
+				print(caixa_cedulas)
+
+
+	else:
+		print('Conta Inválida.')
+
+input('Pressione <Enter> para continuar.')
+limpar()
